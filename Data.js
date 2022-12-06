2ba7dd991c771d5d8e8425ab476becb55aed5a48
MartensGabriel <gabriel.martens.9695@student.uu.se>
2022-12-06 13:43:34 +0100

    Connected users visible in LobbyView

--- Data.js
+++ Data.js
@@ -95,7 +95,7 @@ Data.prototype.createGame=function (quiz){
 }
 
 Data.prototype.joinGame=function(user){
-  console.log("kör")
+
   if(user.joinGameId===this.createdGame.gameId){
     this.createdGame.connectedUsers.push(user)
     console.log("User '"+user.username+"' connected")
--- src/views/LobbyView.vue
+++ src/views/LobbyView.vue
@@ -1,8 +1,11 @@
 <template>
   <body>
 <div>
-hej
-  //{{user.nick}}
+  LOBBY
+  <div v-for="user in this.connectedUsers"
+       v-bind:key="user">
+    {{user.username}}
+  </div>
 
 </div>
 
@@ -20,28 +23,32 @@ const socket = io();
 
 export default {
   name: "LobbyView",
-  props: {
-    user: Object
-  },
 
   data: function () {
     return {
       uiLabels: {},
       lang: "",
+      connectedUsers: [],
 
     }
   },
   created: function () {
+
+    //this.id = this.$route.params.id;
+    //this.nick= this.$route.params.nick;
     this.lang = this.$route.params.lang;
     socket.emit("pageLoaded", this.lang);
     socket.on("init", (labels) => {
       this.uiLabels = labels
 
     })
-    this.id = this.$route.params.id;
-    this.nick= this.$route.params.nick;
+    socket.on('userJoined', (users) => {
+      console.log('user joined')
+      this.connectedUsers = users
+    })
   },
 }
+
 </script>
 
 <style scoped>
